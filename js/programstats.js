0d6e6e214a44fbe51770386a49b1426f5325ec47
Aldanis Vigo <aldanisvigo@gmail.com>
2022-10-24 12:19:39 -0500

    last commit

--- js/programstats.js
+++ js/programstats.js
@@ -2,13 +2,38 @@
     Fetch all the stats from programstats.json  about programs and display them to the user
 */
 fetch('js/data/programstats.json').then(async req=>{
-    let stats = await req.json()
+    let stats = await req.json() //Retrieve all stat objects
+
+    //Retrieve the parent container
     let statsContainer = document.getElementById('stat_numbers')
-    links.forEach(link=>{
-        let linkElement = document.createElement('a')
-        linkElement.innerText = link.title
-        linkElement.href = link.to
-        linkElement.classList.add('nav_link')
-        navLinksContainer.appendChild(linkElement)
+
+    stats.forEach(stat=>{ //Iterate through each stat object
+        //Create the stat element
+        let statElement = document.createElement('div')
+        statElement.classList.add('stat')
+        
+        //Add the stat icon
+        let icon = document.createElement('img')
+        icon.src = stat.icon
+        icon.classList.add('stat_icon')
+        statElement.appendChild(icon)
+
+        //Add the stat number
+        let statNumber = document.createElement('div')
+        statNumber.classList.add('stat_number')
+        statElement.appendChild(statNumber)
+
+        //Add the stat first subheading
+        let firstSubheading = document.createElement('div')
+        firstSubheading.classList.add('stat_subheading_one')
+        statElement.appendChild(firstSubheading)
+
+        //Add the stat second subheading
+        let secondSubheading = document.createElement('div')
+        secondSubheading.classList.add('stat_subheading_two')
+        statElement.appendChild(secondSubheading)
+
+        //Add the stat element to the stats container
+        statsContainer.appendChild(statElement)
     })
 })
